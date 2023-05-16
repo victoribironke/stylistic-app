2b4ca2432d3deeba1f5f59a56402828562242fb3
Victor Ibironke <victoribironke2019@gmail.com>
2023-05-16 15:20:41 +0100

    feat: loaded custom font

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { useCallback } from "react";
 import { homeStyles } from "./styles/home";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
-// check the document for any component you want to use
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
   const { container } = homeStyles;
+  const [fontsLoaded, error] = useFonts({
+    GT: require("./assets/fonts/GTWalsheimPro-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  // check the document for any component you want to use
 
   return (
-    <View style={container}>
+    <View style={container} onLayout={onLayoutRootView}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
