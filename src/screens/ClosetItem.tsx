02c47d24f85bfa695b106b19d13269f1d1726e98
Victor Ibironke <victoribironke2019@gmail.com>
2023-06-05 22:18:32 +0100

    feat: items can be deleted

--- src/screens/ClosetItem.tsx
+++ src/screens/ClosetItem.tsx
@@ -1,17 +1,19 @@
 import OtherHeader from "../components/OtherHeader";
-import { COLORS } from "../../styles/general";
 import { useRecoilValue } from "recoil";
-import { selectedItemState } from "../atoms/atoms";
+import { selectedItemState, userState } from "../atoms/atoms";
 import { closetItemStyles } from "../../styles/closet-item";
 import { Text, View, Image, TouchableOpacity } from "react-native";
+import { arrayRemove, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 const ClosetItem = () => {
-  const { colors, imageURL, subType, type } = useRecoilValue(selectedItemState);
+  const { colors, imageURL, subtype, type, id } =
+    useRecoilValue(selectedItemState);
+  const { uid } = useRecoilValue(userState);
   const {
     container,
     button,
     buttonText,
-    buttonsView,
     colorsContainer,
     colorsView,
     image,
@@ -20,6 +22,12 @@ const ClosetItem = () => {
     typeText,
     typesView,
   } = closetItemStyles;
+
+  const deleteItem = async () => {
+    await updateDoc(doc(db, "users", uid), {
+      closetItems: arrayRemove({ colors, imageURL, subtype, type, id }),
+    });
+  };
 
   return (
     <View style={container}>
@@ -36,18 +44,15 @@ const ClosetItem = () => {
       <View style={typesView}>
         <Text style={typeText}>{type}</Text>
         <View style={separator}></View>
-        <Text style={subTypeText}>{subType}</Text>
+        <Text style={subTypeText}>{subtype}</Text>
       </View>
 
-      <View style={buttonsView}>
-        <TouchableOpacity style={[{ backgroundColor: COLORS.blue }, button]}>
-          <Text style={buttonText}>Edit</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={[{ backgroundColor: "red" }, button]}>
-          <Text style={buttonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity
+        style={[{ backgroundColor: "red" }, button]}
+        onPress={deleteItem}
+      >
+        <Text style={buttonText}>Delete</Text>
+      </TouchableOpacity>
     </View>
   );
 };
